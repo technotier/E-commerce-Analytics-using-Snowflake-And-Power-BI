1126a71cf2acab6bc4754346a07edd780c8d6d1b
Walid Newaz <84900027+technotier@users.noreply.github.com>
2025-12-27 17:00:38 +0600

    Delete table_creation_and_files_uploading.sql

--- table_creation_and_files_uploading.sql
+++
@@ -1,8 +0,0 @@
--- created dim_category
-create table analytics_schema.dim_category as 
-select
-id as category_id,
-initcap(trim(category_name)) as category_name,
-current_timestamp as loaded_at
-from 
-raw_schema.category;
